9ff985e8deb498428763b1e226b12bba3d0c3d47
휘래 김 <hrkim2001@ajou.ac.kr>
2025-02-07 16:30:54 +0900

    develop upgrade

---
+++ src/main/java/com/onrank/server/common/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.onrank.server.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+}
