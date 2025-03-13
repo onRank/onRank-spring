07e0007a77cf34bc9fed8f88f1b68c93a80e6b7f
2ju0123 <2ju1230@gmail.com>
2025-03-13 13:55:57 +0900

    MAinpageStudyResponseDto에 @Getter 추가

--- src/main/java/com/onrank/server/api/dto/study/MainpageStudyResponseDto.java
+++ src/main/java/com/onrank/server/api/dto/study/MainpageStudyResponseDto.java
@@ -2,13 +2,13 @@ package com.onrank.server.api.dto.study;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class MainpageStudyResponseDto {
 
     private String studyName;
-
     private String studyContent;
-
     private String StudyImageUrl;
 }
