05cbb3805a85b5a5d2dd6c6f09faa303c3874837
2ju0123 <2ju1230@gmail.com>
2025-03-27 16:42:32 +0900

    Cookie samesite 설정 - 크로스 사이트 요청 허용

--- src/main/java/com/onrank/server/common/util/CookieUtil.java
+++ src/main/java/com/onrank/server/common/util/CookieUtil.java
@@ -4,12 +4,17 @@ import com.onrank.server.domain.refreshtoken.RefreshToken;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class CookieUtil {
 
@@ -25,14 +30,30 @@ public class CookieUtil {
     /**
      * refresh token 쿠키 생성 및 응답에 추가
      */
-    public void addRefreshTokenCookie(HttpServletResponse response, String name, String refreshToken) {
-        Cookie refreshTokenCookie = new Cookie(name, refreshToken);
-        refreshTokenCookie.setMaxAge(refreshTokenExpiration); // 2시간
-        refreshTokenCookie.setPath(refreshTokenPath);
+//    public void addRefreshTokenCookie(HttpServletResponse response, String name, String refreshToken) {
+//        Cookie refreshTokenCookie = new Cookie(name, refreshToken);
+//        refreshTokenCookie.setMaxAge(refreshTokenExpiration); // 2시간
+//        refreshTokenCookie.setPath(refreshTokenPath);
 //        refreshTokenCookie.setPath("/");
-        refreshTokenCookie.setHttpOnly(true);
-        refreshTokenCookie.setSecure(false);
+//        refreshTokenCookie.setHttpOnly(true);
+//        refreshTokenCookie.setSecure(false);
+//
+//        response.addCookie(refreshTokenCookie);
+//    }
+    public void addRefreshTokenCookie(HttpServletResponse response, String name, String refreshToken) {
+        ResponseCookie cookie = ResponseCookie.from(name, refreshToken)
+                .httpOnly(true)
+                .secure(true)  // HTTPS에서만 전송
+                .path("/")
+                .path(refreshTokenPath)
+                .maxAge(Duration.ofSeconds(refreshTokenExpiration))
+                .domain("onrank.kr")
+                .sameSite("None")  // 크로스 사이트 요청 허용
+                .build();
 
-        response.addCookie(refreshTokenCookie);
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        // 디버깅용 로그 추가
+        log.info("설정된 쿠키: {}", cookie.toString());
     }
 }
