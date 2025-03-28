f89cfc0686483819e9d27a6c2f881182b7df46fa
휘래 김 <hrkim2001@ajou.ac.kr>
2025-03-29 02:26:01 +0900

    change permit.all()

--- src/main/java/com/onrank/server/common/config/SecurityConfig.java
+++ src/main/java/com/onrank/server/common/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
 
                 // request 인증, 인가 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/reissue",
+                        .requestMatchers("/auth/reissue","/oauth2/**", "/auth/add",
                                 "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/v3/api-docs").permitAll() // 인증 없이 접근 가능
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 )
