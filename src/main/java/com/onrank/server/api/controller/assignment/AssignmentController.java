0f71009769028bfb1078c693e3a161179a86df52
2ju0123 <2ju1230@gmail.com>
2025-04-20 21:57:43 +0900

    AssignmentControllerDocs 수정

--- src/main/java/com/onrank/server/api/controller/assignment/AssignmentController.java
+++ src/main/java/com/onrank/server/api/controller/assignment/AssignmentController.java
@@ -1,133 +1,133 @@
-package com.onrank.server.api.controller.assignment;
+    package com.onrank.server.api.controller.assignment;
 
-import com.onrank.server.api.dto.assignment.CreateAssignmentRequest;
-import com.onrank.server.api.dto.assignment.AssignmentDetailResponse;
-import com.onrank.server.api.dto.assignment.AssignmentListResponse;
-import com.onrank.server.api.dto.assignment.CreateSubmissionRequest;
-import com.onrank.server.api.dto.common.ContextResponse;
-import com.onrank.server.api.dto.file.PresignedUrlResponse;
-import com.onrank.server.api.dto.oauth.CustomOAuth2User;
-import com.onrank.server.api.service.assignment.AssignmentService;
-import com.onrank.server.api.service.member.MemberService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+    import com.onrank.server.api.dto.assignment.CreateAssignmentRequest;
+    import com.onrank.server.api.dto.assignment.AssignmentDetailResponse;
+    import com.onrank.server.api.dto.assignment.AssignmentListResponse;
+    import com.onrank.server.api.dto.assignment.CreateSubmissionRequest;
+    import com.onrank.server.api.dto.common.ContextResponse;
+    import com.onrank.server.api.dto.file.PresignedUrlResponse;
+    import com.onrank.server.api.dto.oauth.CustomOAuth2User;
+    import com.onrank.server.api.service.assignment.AssignmentService;
+    import com.onrank.server.api.service.member.MemberService;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.security.core.annotation.AuthenticationPrincipal;
+    import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+    import java.util.List;
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/studies/{studyId}/assignments")
-public class AssignmentController implements AssignmentControllerDocs {
+    @RestController
+    @RequiredArgsConstructor
+    @RequestMapping("/studies/{studyId}/assignments")
+    public class AssignmentController implements AssignmentControllerDocs {
 
-    private final AssignmentService assignmentService;
-    private final MemberService memberService;
+        private final AssignmentService assignmentService;
+        private final MemberService memberService;
 
-    /**
-     * 과제 업로드 - HOST 또는 CREATOR 만 가능
-     */
-    @PostMapping
-    public ResponseEntity<ContextResponse<List<PresignedUrlResponse>>> createAssignment(
-            @PathVariable Long studyId,
-            @RequestBody CreateAssignmentRequest request,
-            @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+        /**
+         * 과제 업로드 - HOST 또는 CREATOR 만 가능
+         */
+        @PostMapping
+        public ResponseEntity<ContextResponse<List<PresignedUrlResponse>>> createAssignment(
+                @PathVariable Long studyId,
+                @RequestBody CreateAssignmentRequest request,
+                @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
 
-        // CREATOR, HOST 만 가능
-        if (!memberService.isMemberCreatorOrHost(oAuth2User.getName(), studyId)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
-        return ResponseEntity.status(HttpStatus.CREATED).body(assignmentService.createAssignment(oAuth2User.getName(), studyId, request));
-    }
-
-    /**
-     * 과제 목록 조회 - 멤버 기준
-     */
-    @GetMapping
-    public ResponseEntity<ContextResponse<List<AssignmentListResponse>>> getAssignments(
-            @PathVariable Long studyId,
-            @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
-
-        // 스터디 멤버만 가능
-        if (!memberService.isMemberInStudy(oAuth2User.getName(), studyId)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            // CREATOR, HOST 만 가능
+            if (!memberService.isMemberCreatorOrHost(oAuth2User.getName(), studyId)) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
+            return ResponseEntity.status(HttpStatus.CREATED).body(assignmentService.createAssignment(oAuth2User.getName(), studyId, request));
         }
 
-        return ResponseEntity.ok(assignmentService.getAssignments(oAuth2User.getName(), studyId));
-    }
+        /**
+         * 과제 목록 조회 - 멤버 기준
+         */
+        @GetMapping
+        public ResponseEntity<ContextResponse<List<AssignmentListResponse>>> getAssignments(
+                @PathVariable Long studyId,
+                @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
 
-    /**
-     * 과제 상세 조회 - 멤버 기준
-     */
-    @GetMapping("/{assignmentId}")
-    public ResponseEntity<ContextResponse<AssignmentDetailResponse>> getAssignmentDetail(
-            @PathVariable Long studyId,
-            @PathVariable Long assignmentId,
-            @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+            // 스터디 멤버만 가능
+            if (!memberService.isMemberInStudy(oAuth2User.getName(), studyId)) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
 
-        // 스터디 멤버만 가능
-        if (!memberService.isMemberInStudy(oAuth2User.getName(), studyId)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            return ResponseEntity.ok(assignmentService.getAssignments(oAuth2User.getName(), studyId));
         }
 
-        return ResponseEntity.ok(assignmentService.getAssignmentDetail(oAuth2User.getName(), studyId, assignmentId));
-    }
+        /**
+         * 과제 상세 조회 - 멤버 기준
+         */
+        @GetMapping("/{assignmentId}")
+        public ResponseEntity<ContextResponse<AssignmentDetailResponse>> getAssignmentDetail(
+                @PathVariable Long studyId,
+                @PathVariable Long assignmentId,
+                @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
 
-    /**
-     * 제출물 업로드 - 멤버 기준
-     */
-    @PostMapping("/{assignmentId}")
-    public ResponseEntity<ContextResponse<List<PresignedUrlResponse>>> createSubmission(
-            @PathVariable Long studyId,
-            @PathVariable Long assignmentId,
-            @RequestBody CreateSubmissionRequest request,
-            @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+            // 스터디 멤버만 가능
+            if (!memberService.isMemberInStudy(oAuth2User.getName(), studyId)) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
 
-        // 스터디 멤버만 가능
-        if (!memberService.isMemberInStudy(oAuth2User.getName(), studyId)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            return ResponseEntity.ok(assignmentService.getAssignmentDetail(oAuth2User.getName(), studyId, assignmentId));
         }
 
-        return ResponseEntity.status((HttpStatus.CREATED)).body(assignmentService.createSubmission(oAuth2User.getName(), studyId, assignmentId, request));
+        /**
+         * 제출물 업로드 - 멤버 기준
+         */
+        @PostMapping("/{assignmentId}")
+        public ResponseEntity<ContextResponse<List<PresignedUrlResponse>>> createSubmission(
+                @PathVariable Long studyId,
+                @PathVariable Long assignmentId,
+                @RequestBody CreateSubmissionRequest request,
+                @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+
+            // 스터디 멤버만 가능
+            if (!memberService.isMemberInStudy(oAuth2User.getName(), studyId)) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
+
+            return ResponseEntity.status((HttpStatus.CREATED)).body(assignmentService.createSubmission(oAuth2User.getName(), studyId, assignmentId, request));
+        }
     }
-}
 
-// 제출물 업로드 (멤버 기준)
-// request: 제출물 내용 / 새로 제출한 제출물 파일 List(파일 이름)
-// response: 제출물 파일 List(FileMetadataDto)
+    // 제출물 업로드 (멤버 기준)
+    // request: 제출물 내용 / 새로 제출한 제출물 파일 List(파일 이름)
+    // response: 제출물 파일 List(FileMetadataDto)
 
-// 제출물 수정 (멤버 기준 / SUBMITTED)
-// request: 제출물 내용 / 기존 제출물 파일 List(FileMetadataDto), 새로 제출한 제출물 파일 List(파일 이름)
-// response: 새로 제출한 제출물 파일 List(FileMetadataDto)
+    // 제출물 수정 (멤버 기준 / SUBMITTED)
+    // request: 제출물 내용 / 기존 제출물 파일 List(FileMetadataDto), 새로 제출한 제출물 파일 List(파일 이름)
+    // response: 새로 제출한 제출물 파일 List(FileMetadataDto)
 
-/*--------------------------------------------------*/
+    /*--------------------------------------------------*/
 
-// 과제 수정 페이지 (관리자 기준)
-// request:
-// response: 제목, 지시사항, 마감기한, 최대포인트 / 과제 파일 List(FileMetadataDto)
+    // 과제 수정 페이지 (관리자 기준)
+    // request:
+    // response: 제목, 지시사항, 마감기한, 최대포인트 / 과제 파일 List(FileMetadataDto)
 
-// 과제 수정 (관리자 기준)
-// request: 제목, 지시사항, 마감기한, 최대포인트 / 기존 과제 파일 List(FileMetadataDto) / 새로 업로드한 과제 파일 List(파일 이름)
-// response:새로 업로드한 제출물 파일 List(FileMetadataDto)
+    // 과제 수정 (관리자 기준)
+    // request: 제목, 지시사항, 마감기한, 최대포인트 / 기존 과제 파일 List(FileMetadataDto) / 새로 업로드한 과제 파일 List(파일 이름)
+    // response:새로 업로드한 제출물 파일 List(FileMetadataDto)
 
-// 과제 삭제 (관리자 기준)
-// request:
-// response:
+    // 과제 삭제 (관리자 기준)
+    // request:
+    // response:
 
-/*--------------------------------------------------*/
+    /*--------------------------------------------------*/
 
-// 제출물 목록 조회 (관리자 기준)
-// request:
-// response: 과제 정보(제목, 마감기한) / 제출물 List(제출물 ID, 멤버 정보(ID, 이름, email), 제출여부, 제출 일시(SUIBMITTED, SCORED), 점수(SCORED))
+    // 제출물 목록 조회 (관리자 기준)
+    // request:
+    // response: 과제 정보(제목, 마감기한) / 제출물 List(제출물 ID, 멤버 정보(ID, 이름, email), 제출여부, 제출 일시(SUIBMITTED, SCORED), 점수(SCORED))
 
-// 제출물 상세 조회 (관리자 기준 / 채점 페이지 / SUBMITTED, SCORED)
-// request:
-// response:
-// SUBMITTED: 과제 정보(제목, 마감기한) / 멤버 정보(ID, 이름, email) 제출 일시
-// SCORED: + 점수, 코멘트
+    // 제출물 상세 조회 (관리자 기준 / 채점 페이지 / SUBMITTED, SCORED)
+    // request:
+    // response:
+    // SUBMITTED: 과제 정보(제목, 마감기한) / 멤버 정보(ID, 이름, email) 제출 일시
+    // SCORED: + 점수, 코멘트
 
-// 제출물 채점 (관리자 기준)
-// request: 점수, 코멘트
-// response:
-// 멤버 엔티티 과제 총점수 속성 업데이트
+    // 제출물 채점 (관리자 기준)
+    // request: 점수, 코멘트
+    // response:
+    // 멤버 엔티티 과제 총점수 속성 업데이트
--- src/main/java/com/onrank/server/api/controller/assignment/AssignmentControllerDocs.java
+++ src/main/java/com/onrank/server/api/controller/assignment/AssignmentControllerDocs.java
@@ -1,6 +1,9 @@
 package com.onrank.server.api.controller.assignment;
 
+import com.onrank.server.api.dto.assignment.AssignmentDetailResponse;
+import com.onrank.server.api.dto.assignment.AssignmentListResponse;
 import com.onrank.server.api.dto.assignment.CreateAssignmentRequest;
+import com.onrank.server.api.dto.assignment.CreateSubmissionRequest;
 import com.onrank.server.api.dto.common.ContextResponse;
 import com.onrank.server.api.dto.file.PresignedUrlResponse;
 import com.onrank.server.api.dto.oauth.CustomOAuth2User;
@@ -31,6 +34,51 @@ public interface AssignmentControllerDocs {
     ResponseEntity<ContextResponse<List<PresignedUrlResponse>>> createAssignment(
             @Parameter(description = "스터디 ID", example = "1") @PathVariable Long studyId,
             @RequestBody CreateAssignmentRequest request,
+            @AuthenticationPrincipal CustomOAuth2User oAuth2User
+    );
+
+    @Operation(
+            summary = "과제 목록 조회",
+            description = "스터디에 등록된 과제 목록을 조회합니다. 스터디에 참여한 멤버만 접근할 수 있습니다.",
+            security = {@SecurityRequirement(name = "bearer-key")}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "과제 목록 조회 성공"),
+            @ApiResponse(responseCode = "403", description = "스터디 멤버가 아님")
+    })
+    ResponseEntity<ContextResponse<List<AssignmentListResponse>>> getAssignments(
+            @Parameter(description = "스터디 ID", example = "1") @PathVariable Long studyId,
+            @AuthenticationPrincipal CustomOAuth2User oAuth2User
+    );
+
+    @Operation(
+            summary = "과제 상세 조회",
+            description = "특정 과제의 상세 정보를 조회합니다. 스터디에 참여한 멤버만 접근할 수 있습니다.",
+            security = {@SecurityRequirement(name = "bearer-key")}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "과제 상세 조회 성공"),
+            @ApiResponse(responseCode = "403", description = "스터디 멤버가 아님")
+    })
+    ResponseEntity<ContextResponse<AssignmentDetailResponse>> getAssignmentDetail(
+            @Parameter(description = "스터디 ID", example = "1") @PathVariable Long studyId,
+            @Parameter(description = "과제 ID", example = "10") @PathVariable Long assignmentId,
+            @AuthenticationPrincipal CustomOAuth2User oAuth2User
+    );
+
+    @Operation(
+            summary = "제출물 업로드",
+            description = "과제에 대한 제출물을 업로드합니다. 스터디 멤버만 접근할 수 있습니다.",
+            security = {@SecurityRequirement(name = "bearer-key")}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "제출물 업로드 성공"),
+            @ApiResponse(responseCode = "403", description = "스터디 멤버가 아님")
+    })
+    ResponseEntity<ContextResponse<List<PresignedUrlResponse>>> createSubmission(
+            @Parameter(description = "스터디 ID", example = "1") @PathVariable Long studyId,
+            @Parameter(description = "과제 ID", example = "10") @PathVariable Long assignmentId,
+            @RequestBody CreateSubmissionRequest request,
             @AuthenticationPrincipal CustomOAuth2User oAuth2User
     );
 }
