e179d69f37cd6ab3ab9b891c082e58e5e9c08997
휘래 김 <hramst0618@gmail.com>
2025-05-01 16:18:01 +0900

    CustomException 테스트용 Controller 추가

--- src/main/java/com/onrank/server/api/controller/TestController.java
+++ src/main/java/com/onrank/server/api/controller/TestController.java
@@ -2,22 +2,29 @@ package com.onrank.server.api.controller;
 
 
 import com.onrank.server.common.exception.CustomException;
-import com.onrank.server.domain.member.Member;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.onrank.server.common.exception.CustomErrorInfo.NOT_STUDY_MEMBER;
+import static com.onrank.server.common.exception.CustomErrorInfo.*;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TestController {
 
     @GetMapping("/test/exception")
-    public ResponseEntity<Member> test() {
-        throw new CustomException(NOT_STUDY_MEMBER);
+    public void exception1() {  // @ExceptionHandler({Exception.class})
+        throw new IllegalStateException("State Exception...");
+    }
+
+    @GetMapping("/test/exception/custom1")
+    public void custom_exception1(){  // @ExceptionHandler(CustomException.class)
+        throw new CustomException(ACCESS_DENIED);
+    }
+
+    @GetMapping("/test/exception/custom2")
+    public ResponseEntity<Object> custom_exception2(){  // @ExceptionHandler(CustomException.class)
+        throw new CustomException(STUDENT_NOT_FOUND);
     }
 }
--- src/main/java/com/onrank/server/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/onrank/server/common/exception/GlobalExceptionHandler.java
@@ -1,24 +1,19 @@
 package com.onrank.server.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @ExceptionHandler({CustomException.class})
-    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Object> handleCustomException(CustomException ex) {
 
         CustomErrorInfo customErrorInfo = ex.getCustomErrorInfo();
         ErrorResponse response = ErrorResponse.builder()
@@ -37,21 +32,5 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR) // 500
                 .body(Map.of("message", ex.getMessage()));
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex,
-            HttpHeaders headers,
-            HttpStatusCode status,
-            WebRequest request) {
-
-        ErrorResponse response = ErrorResponse.builder()
-                .message(ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage())
-                .build();
-
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST) // 400
-                .body(response);
     }
 }
