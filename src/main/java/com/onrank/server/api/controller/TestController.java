15d2b591821b7365db9090ded643f88ab8063a78
휘래 김 <hramst0618@gmail.com>
2025-05-01 13:41:44 +0900

    CustomException 테스트용 Controller 추가

---
+++ src/main/java/com/onrank/server/api/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.onrank.server.api.controller;
+
+
+import com.onrank.server.common.exception.CustomException;
+import com.onrank.server.domain.member.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.onrank.server.common.exception.CustomErrorInfo.NOT_STUDY_MEMBER;
+
+@Slf4j
+@RestController
+@RequestMapping("/test/exception")
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<Member> test() {
+        throw new CustomException(NOT_STUDY_MEMBER);
+    }
+
+}
