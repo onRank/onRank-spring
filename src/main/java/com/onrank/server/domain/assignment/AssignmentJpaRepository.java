71287f06fff65ba68a7133ea79b1b4fb7d5ccf9c
휘래 김 <hramst0618@gmail.com>
2025-05-08 00:52:39 +0900

    @Scheduled

--- src/main/java/com/onrank/server/domain/assignment/AssignmentJpaRepository.java
+++ src/main/java/com/onrank/server/domain/assignment/AssignmentJpaRepository.java
@@ -1,11 +1,13 @@
 package com.onrank.server.domain.assignment;
 
-import com.onrank.server.domain.study.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AssignmentJpaRepository extends JpaRepository<Assignment, Long> {
 
     List<Assignment> findByStudyStudyId(Long studyId);
+
+    List<Assignment> findByAssignmentDueDateBetween(LocalDateTime start, LocalDateTime end);
 }
