914acbee5f66d1480f712be9ff71b298f2f457fe
휘래 김 <hramst0618@gmail.com>
2025-06-05 19:51:18 +0900

    refactor:username log 추가

--- src/main/java/com/onrank/server/common/security/jwt/JWTOAuth2AuthenticationFilter.java
+++ src/main/java/com/onrank/server/common/security/jwt/JWTOAuth2AuthenticationFilter.java
@@ -1,10 +1,11 @@
 package com.onrank.server.common.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onrank.server.api.dto.auth.CustomOAuth2User;
 import com.onrank.server.api.service.student.StudentService;
+import com.onrank.server.common.exception.CustomErrorInfo;
+import com.onrank.server.common.exception.CustomException;
 import com.onrank.server.common.util.JWTUtil;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
 import io.micrometer.common.lang.NonNullApi;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -20,7 +21,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -31,15 +34,16 @@ public class JWTOAuth2AuthenticationFilter extends OncePerRequestFilter {
     private final JWTUtil jwtUtil;
     private final StudentService studentService;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
 
+        // Access Token 추출
         String authHeader = request.getHeader("Authorization");
-
-        // access token 이 없으면 필터 건너뛰기
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             log.info("Access token not found");
             filterChain.doFilter(request, response);
@@ -47,48 +51,35 @@ public class JWTOAuth2AuthenticationFilter extends OncePerRequestFilter {
         }
 
         String accessToken = authHeader.substring(7);
+        String username;
+        String email;
 
-        // username은 "google aSdFqWeR..."과 같이 되어 있음
-        String username = jwtUtil.getUsername(accessToken);
+        try {
+            // Access Token이 유효한지 확인
+            jwtUtil.validateAccessToken(accessToken);
+            username = jwtUtil.getUsername(accessToken);
+            email = jwtUtil.getEmail(accessToken);
 
-        // 등록된 회원이 아니면 필터 건너뛰기
+            if(!"access".equals(jwtUtil.getCategory(accessToken))) {
+                throw new CustomException(CustomErrorInfo.INVALID_ACCESS_TOKEN);
+            }
+        } catch (CustomException e) {
+            writeErrorResponse(response, e.getCustomErrorInfo());
+            return;
+        }
+
+        // 신규 유저는 SecurityContext 등록하지 않음 (필터 건너뛰기)
         if (studentService.checkIfNewUser(username)) {
             log.info("New user logged in");
             filterChain.doFilter(request, response);
             return;
         }
 
-        // 토큰 만료 여부 및 유효성 확인
-        try {
-            jwtUtil.isTokenExpired(accessToken);
-
-        } catch (ExpiredJwtException ex) {
-
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.setContentType("application/json");
-            response.getWriter().write("{\"error\": \"access token expired\"}");
-            return;
-        } catch (JwtException | IllegalArgumentException e) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.setContentType("application/json");
-            response.getWriter().write("{\"error\": \"Invalid token\"}");
-            return;
-        }
-
-        // 토큰이 access인지 확인 (발급시 페이로드에 명시)
-        String category = jwtUtil.getCategory(accessToken);
-
-        if (!category.equals("access")) {
-
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.setContentType("application/json");
-            response.getWriter().write("{\"error\": \"Invalid token\"}");
-            return;
-        }
-
         String[] parts = username.split(" ");
         String authorizedClientRegistrationId = parts[0];
 
+        log.info("authorizedClientRegistrationId: {}", authorizedClientRegistrationId);
+        log.info("username: {}", username);
         // "ROLE_USER" 권한 부여
         Collection<? extends GrantedAuthority> authorities = studentService.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Student not found"))
@@ -100,7 +91,6 @@ public class JWTOAuth2AuthenticationFilter extends OncePerRequestFilter {
         Map<String, Object> attributes = new HashMap<>();
         attributes.put("username", username);
 
-        String email = jwtUtil.getEmail(accessToken);
         attributes.put("email", email);
 
         CustomOAuth2User customOAuth2User = new CustomOAuth2User(authorities, attributes, username, email);
@@ -114,5 +104,14 @@ public class JWTOAuth2AuthenticationFilter extends OncePerRequestFilter {
         log.info("SecurityContextHolder: {}", SecurityContextHolder.getContext());
 
         filterChain.doFilter(request, response);
+    }
+
+    private void writeErrorResponse(HttpServletResponse response, CustomErrorInfo errorInfo) throws IOException {
+        response.setStatus(errorInfo.getHttpStatus().value());
+        response.setContentType("application/json");
+        response.getWriter().write(String.format(
+                "{\"code\": \"%s\", \"message\": \"%s\"}",
+                errorInfo.name(), errorInfo.getMessage())
+        );
     }
 }
